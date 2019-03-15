07e104679642135443dc57f2e01b47d5324d8e0f
Kayahan Saritas <kayahansaritas@Kayahans-MacBook-Pro.local>
2019-03-14 23:18:06 -0400

    metropolis

--- loop.py
+++ loop.py
@@ -10,12 +10,27 @@ from atoms import Atom
 from box import Box
 from stats import Stats
 
+class Particles():
+    def __init__(self, n = 1):
+        self.n = n
+        self.box = Box()
+        self.density = Density()
+        self.p = generate_particles()
+    #end def
+    def set_particles(self, n):
+        self.n = n
+    #end def
+    def generate_particles():
+        n = self.n
+        box = self.box
+        return random(n)
+    
 class vmc_loop():
     def __init__(self, nstep = 20, nblock = 10):
         self.eq        = False
         self.nstep     = nstep
         self.block     = nblock
-        self.particles = []
+        self.particles = Particles()
         self.d         = 0.2
         self.box       = None
         self.wave_func = None
@@ -43,9 +58,13 @@ class vmc_loop():
         #end if
     #end def move
     
-    def evaluate_energy(self, eq=False):
+    def evaluate_energy(self):
         for nb in range(self.block):
             for ns in range(self.nstep):
                 for p in self.particles:
                     if p.chg < 0.0:
-                        pnew = move(p, 
+                        pnew = move(p)
+                        
+
+for i in range(n):
+    x[i] = random.rand(1)
